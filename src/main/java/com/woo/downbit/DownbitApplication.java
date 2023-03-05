82c9d5b8aeb73b5a5c64d9a5a9f17214bdfe0fa8
woo <chlguddn7@gmail.com>
2023-03-05 14:12:35 +0900

    Initial commit

---
+++ src/main/java/com/woo/downbit/DownbitApplication.java
@@ -0,0 +1,13 @@
+package com.woo.downbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownbitApplication.class, args);
+	}
+
+}
