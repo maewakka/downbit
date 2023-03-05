659aa7c07171300901e5c481927112e2a96e1061
woo <chlguddn7@gmail.com>
2023-03-05 14:28:13 +0900

    기본 Entity 생성

---
+++ src/main/java/com/woo/downbit/entity/trade/AssetManagement.java
@@ -0,0 +1,12 @@
+package com.woo.downbit.entity.trade;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class AssetManagement {
+}
---
+++ src/main/java/com/woo/downbit/entity/user/User.java
@@ -0,0 +1,12 @@
+package com.woo.downbit.entity.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+}
